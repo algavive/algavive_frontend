a5aca4a42e1ea31c8846d335f1b60ff6806034ec
SublimeBit <SublimeBit@users.noreply.github.com>
2026-05-31 18:58:42 +0300

    changed vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: 'static',  
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  server: {
+    port: 5173,
+  },
+});
