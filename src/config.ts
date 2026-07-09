99a187f4bacfeca3f15553f133712808f2b44de8
SublimeBit <SublimeBit@users.noreply.github.com>
2026-07-09 18:30:05 +0300

    Updated Home Page and README

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const STATIC_LOCATION: string = ""
 //Публичные ключи
-export const GOOGLE_CLIENT_ID: string = "55121685745-c31lj4mjqtrfobfthu3i8p3l1gau4bkf.apps.googleusercontent.com"; // ключ от google console для oautn
+export const GOOGLE_CLIENT_ID: string = "55121685745-c31lj4mjqtrfobfthu3i8p3l1gau4bkf.apps.googleusercontent.com"; // публичный ключ от google console для oauth
 //фронтдев мод
 export const FRONTDEV_MODE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.hostname === '0.0.0.0';
 
